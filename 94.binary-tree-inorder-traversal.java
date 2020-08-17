151a74cdeb30f40a3ea5eb4b8228c9d9cc4971b9
wangmingxuan <wangmingxuan414@icloud.com>
2020-08-17 08:56:54 +0800

    8.16

---
+++ 94.binary-tree-inorder-traversal.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (63.30%)
+ * Likes:    3369
+ * Dislikes: 140
+ * Total Accepted:    765.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public ArrayList<Integer> trabverseList = new ArrayList<>();
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null)
+            return trabverseList;
+        inorderTraversal(root.left);
+        trabverseList.add(root.val);
+        inorderTraversal(root.right);
+        return trabverseList;
+    }
+}
+// @lc code=end
+
